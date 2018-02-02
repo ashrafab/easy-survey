f1a7a936d5a4ed6c42c6f67b98d77cab9793f34b
boujnane monssef <m-heroine@hotmail.com>
2018-02-02 15:35:45 +0000

    loading port from .env

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = require("./server/app.js");
 
-const PORT = 8888;
+const { PORT = 3000 } = process.env;
 app.listen(PORT, () => {
   console.log(`server is listening on port ${PORT}`);
 });
--- server/app.js
+++ server/app.js
@@ -1,4 +1,8 @@
 const express = require("express");
+
+// load env variables into process.env
+require("dotenv").config();
+
 const app = express();
 
 module.exports = app;
