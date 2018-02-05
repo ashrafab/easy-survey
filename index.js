bef787d389b105c1c8ab7b4b258043aa2992cea4
boujnane monssef <m-heroine@hotmail.com>
2018-02-05 13:23:33 +0000

    handling mongoose connection error

--- index.js
+++ index.js
@@ -8,12 +8,16 @@ const { PORT = 3000, MONGO_DB_URI } = process.env;
 const compiler = webpack(webpackConfig);
 compiler.run(function(err, stats) {
   if (err || stats.hasErrors()) {
-    throw new Error(err);
+    throw new Error(stats.toString());
   }
   console.log("webpack build finished");
 });
 
 mongoose.connect(MONGO_DB_URI);
+mongoose.connection.on("error", err => {
+  throw err;
+});
+
 app.listen(PORT, () => {
   console.log(`server is listening on port ${PORT}`);
 });
